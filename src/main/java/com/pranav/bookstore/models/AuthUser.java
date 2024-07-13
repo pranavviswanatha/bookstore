347eda1a74aa59e49ebca2417a4206a485ef62ed
Pranav <pranav279.v@gmail.com>
2024-07-13 23:24:08 +0530

    security2

---
+++ src/main/java/com/pranav/bookstore/models/AuthUser.java
@@ -0,0 +1,42 @@
+package com.pranav.bookstore.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Set;
+
+@Document(collection = "authUsers")
+public class AuthUser {
+    @Id
+    private String id;
+    @Indexed
+    private String username;
+    private String password;
+
+    // getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
