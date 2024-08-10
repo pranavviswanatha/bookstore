b8e7786268070b0b292825ec91fc499cac1a7340
Pranav <pranav279.v@gmail.com>
2024-08-10 19:38:43 +0530

    changes

--- src/main/java/com/pranav/bookstore/controller/Controller.java
+++ src/main/java/com/pranav/bookstore/controller/Controller.java
@@ -3,31 +3,23 @@ package com.pranav.bookstore.controller;
 import com.pranav.bookstore.service.SearchRepo;
 import com.pranav.bookstore.repository.Library;
 import com.pranav.bookstore.models.Book;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping (value = "/home")
 public class Controller {
     @Autowired
     Library library;
-
+    @Autowired
     private final SearchRepo searchRepo;
 
-    @Autowired
-    public Controller(SearchRepo searchRepo) {
-        this.searchRepo = searchRepo;
-    }
-
-    @GetMapping(value = "/allBooks")
-    public List<Book> getAllBooks () {
-        return library.findAll();
-    }
-
-    @GetMapping(value = "/booksMatching")
-    public List<Book> search(@RequestParam(required = false) String searchText,
+    @GetMapping(value = "/getBooks")
+    public List<Book> searchMatching(@RequestParam(required = false) String searchText,
                              @RequestParam(required = false) List<String> genres){
         return searchRepo.searchBooks(searchText, genres);
     }
